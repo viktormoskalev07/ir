1701659c5a8b7a4a577ea26ba9e38ef0ce42002e
unknown <viktormoskalev07@gmail.com>
2021-06-25 18:39:17 +0300

    f

--- src/js/main.js
+++ src/js/main.js
@@ -97,46 +97,46 @@ function pagescroll(){
 
 
 //отправка формы  
-let form = document.querySelectorAll('.order-form');
+// let form = document.querySelectorAll('.order-form');
 
-const modal = document.getElementById("modal");
+// const modal = document.getElementById("modal");
 
-function ajaxform(evt) {
-  gtag('event', 'submit', {
-    'event_category': 'Form'
-  });
-  evt.preventDefault();
-  let formstatus = this.querySelector('.formstatus');
-  formstatus.innerHTML = '<class="load-form">Соедиенеие ...';
+// function ajaxform(evt) {
+//   gtag('event', 'submit', {
+//     'event_category': 'Form'
+//   });
+//   evt.preventDefault();
+//   let formstatus = this.querySelector('.formstatus');
+//   formstatus.innerHTML = '<class="load-form">Соедиенеие ...';
 
-  let formData = {
-    desc: this.querySelector('input[name="description"]').value,
-    name: this.querySelector('input[name="name"]').value,
+//   let formData = {
+//     desc: this.querySelector('input[name="description"]').value,
+//     name: this.querySelector('input[name="name"]').value,
 
-    phone: this.querySelector('input[name="phone"]').value
+//     phone: this.querySelector('input[name="phone"]').value
 
-  };
-  console.log(formData);
-  let request = new XMLHttpRequest();
+//   };
+//   console.log(formData);
+//   let request = new XMLHttpRequest();
 
-  request.addEventListener('load', function () {
+//   request.addEventListener('load', function () {
 
-    formstatus.innerHTML = 'Ваша заявка успешно отправлена, ожидайте звонка';
+//     formstatus.innerHTML = 'Ваша заявка успешно отправлена, ожидайте звонка';
 
-  });
+//   });
 
-  request.open('POST', '/mail.php', true);
-  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-  request.send('name=' + encodeURIComponent(formData.name) + '&phone=' + encodeURIComponent(formData.phone) + '&desc=' + encodeURIComponent(formData.desc));
-
-
-};
+//   request.open('POST', '/mail.php', true);
+//   request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+//   request.send('name=' + encodeURIComponent(formData.name) + '&phone=' + encodeURIComponent(formData.phone) + '&desc=' + encodeURIComponent(formData.desc));
 
 
+// };
 
-for (i = 0; i < form.length; i++) {
-  form[i].addEventListener('submit', ajaxform);
-} 
+
+
+// for (i = 0; i < form.length; i++) {
+//   form[i].addEventListener('submit', ajaxform);
+// } 
 //отправка формы
 
 // accordion
@@ -151,16 +151,16 @@ for (i = 0; i < form.length; i++) {
             for (let i = 0; i < items.length; i++) {    
               
               titles[i].addEventListener('click', function(){ 
-                let close = true;
+               
                 if(this.classList.contains('is-active__js')){
-                  close=false;
+                  return '';
                 }
                     for (let i = 0; i < titles.length; i++) {
                       titles[i].classList.remove('is-active__js'); 
                     }
-                    if(close){
+                   
                        this.classList.add('is-active__js');
-                    }
+                   
                
               });
             } 
