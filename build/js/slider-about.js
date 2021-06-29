84f22103406e029e2d614e940d8dc7e1b468dc4f
unknown <viktormoskalev07@gmail.com>
2021-06-29 21:33:06 +0300

    build

--- build/js/slider-about.js
+++
@@ -1,1 +0,0 @@
-const swiperMilestones=new Swiper(".swiper-milestones",{loop:!1,navigation:{nextEl:".milestones-next",prevEl:".milestones-prev"},breakpoints:{320:{slidesPerView:1.6},480:{slidesPerView:2},1024:{slidesPerView:3},1600:{slidesPerView:4}}});
